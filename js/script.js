e65dd0719725c23cdb30897069dffecee015ccac
Drew <drew@finstrom.us>
2015-06-05 09:43:08 -0700

    add base price for pizza based on given size; add to price when adding toppings

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-//raw js
+// pizza object
 var Pizza = function(size) {
   "use strict";
   this.toppings = [];
   this.size = size;
+  switch(size) {
+    case "large":
+      this.price = 15.00;
+      break;
+  }
 };
 
 Pizza.prototype.addTopping = function(toppingName) {
   "use strict";
   this.toppings.push(toppingName);
+  this.price += 0.50;
 };
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -3,10 +3,21 @@ describe("pizza", function() {
   describe("size", function() {
     it("has a size", function() {
       var pizza1 = new Pizza("large");
-      expect(pizza1.size).to.eql("large");
+      expect(pizza1.size).to.equal("large");
     });
   });
-
+  describe("price", function() {
+    it("has a base price (for a given size)", function() {
+      var pizza1 = new Pizza("large");
+      expect(pizza1.price).to.equal(15.00);
+    });
+    it("increases in price as toppings are added", function() {
+      var pizza1 = new Pizza("large");
+      pizza1.addTopping("mushroom");
+      pizza1.addTopping("ham");
+      expect(pizza1.price).to.equal(16.00);
+    });
+  });
   describe("toppings", function() {
     it("has toppings", function() {
       var pizza1 = new Pizza("large");
